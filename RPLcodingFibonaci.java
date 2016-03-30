c17634f4d7ed9fc9cf9cc03da71f750bd4d21c73
Yurvan Igo Wibowo <yurvan160@gmail.com>
2016-03-30 10:18:38 +0700

    Added files via upload

---
+++ RPLcodingFibonaci.java
@@ -0,0 +1,34 @@
+
+package rplcodingfibonaci;
+import java.util.Scanner;
+
+
+
+/**
+ *
+ * @author Praktikan
+ */
+public class RPLcodingFibonaci {
+
+
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.print("Masukkanpa deret fibonacci ? ");
+		int n = s.nextInt();
+		for(int i=1;i<=n;i++)
+			System.out.print(fibo(i) + " ");
+		System.out.println();
+	}
+
+	private static int fibo(int n)
+	{
+		int f;
+		if(n==1 || n==2)
+			f=1;
+		else
+			f = fibo(n-1) + fibo(n-2);
+		return f;
+	}
+}
+
